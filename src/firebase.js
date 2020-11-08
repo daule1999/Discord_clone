8c0129ed92f53698718f1d2ee7c98545ddd6e597
Dauleshwar kumar <37247963+daule1999@users.noreply.github.com>
2020-11-08 11:46:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDbEOcsegXjHsicpYH77entR523K3oH5_M",
+  apiKey: "AIzagXjHsicntR52oH5_M",
   authDomain: "discordclone-91a30.firebaseapp.com",
   databaseURL: "https://discordclone-91a30.firebaseio.com",
-  projectId: "discordclone-91a30",
-  storageBucket: "discordclone-91a30.appspot.com",
-  messagingSenderId: "655213120426",
-  appId: "1:655213120426:web:deead7a87365cac636b9d3",
-  measurementId: "G-DM02QS1P71",
+  projectId: "discord",
+  storageBucket: "discordclom",
+  messagingSenderId: "6552",
+  appId: "1:655213a87365cac636b9d3",
+  measurementId: "G-DM01",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
